0c33644f824b751ccb84357413edf9240be19d20
Engin Gökmen <engin.gokmen@clevel.team>
2020-05-10 09:05:48 +0300

    server port is changed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 80;
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
